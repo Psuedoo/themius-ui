7a50aeb0267c2afd9f61b853d1c267531e8d16f2
Psuedoo <forthewin720@gmail.com>
2022-11-22 23:43:57 -0500

    add fetching users

--- pages/regulars.js
+++ pages/regulars.js
@@ -1,3 +1,36 @@
+import PocketBase from "pocketbase";
+import { useEffect, useState } from "react";
+
+async function getRegulars() {
+  const pb = new PocketBase("https://pocketbase.theflinners.com");
+
+  const adminData = await pb.admins.authWithPassword("email", "password");
+
+  const data = await pb.collection("chatters").getFullList();
+  return data;
+}
+
 export default function Regulars() {
-  return <h1>Regulars page</h1>;
+  const [regulars, setRegulars] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    const fetchRegulars = async () => {
+      const regulars = await getRegulars();
+      setRegulars(regulars);
+    };
+    // fetchRegulars();
+    setLoading(false);
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        regulars.map((regular) => {
+          return <div key={regular.id}>{regular.username}</div>;
+        })
+      )}
+    </div>
+  );
 }
