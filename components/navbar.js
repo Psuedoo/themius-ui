a2df725053955d3bf1fe04137ad908adb30e3168
Psuedoo <forthewin720@gmail.com>
2022-11-22 23:10:48 -0500

    remove account button

--- components/navbar.js
+++ components/navbar.js
@@ -1,88 +1,87 @@
 import {
-  Avatar,
-  Box,
-  Button,
-  Text,
-  useRadio,
-  useRadioGroup,
+    Avatar,
+    Box,
+    Button,
+    Text,
+    useRadio,
+    useRadioGroup,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { useState } from "react";
 
 function NavButton(props) {
-  const { state, getCheckboxProps, getLabelProps } = useRadio(props);
-  return (
-    <Link href={props.href} className="flex flex-initial w-full">
-      <Box
-        h={70}
-        w={175}
-        borderRadius="10"
-        className="flex w-full justify-center items-center mt-5 "
-        boxShadow="lg"
-        _hover={{ boxShadow: "inner" }}
-        bg={state.isChecked ? "transparent" : "transparent"}
-        {...getCheckboxProps()}
-      >
-        <Text
-          color="white"
-          fontSize="xl"
-          className="w-100%"
-          {...getLabelProps()}
-        >
-          {props.label}
-        </Text>
-      </Box>
-    </Link>
-  );
+    const { state, getCheckboxProps, getLabelProps } = useRadio(props);
+    return (
+        <Link href={props.href} className="flex flex-initial w-full">
+            <Box
+                h={70}
+                w={175}
+                borderRadius="10"
+                className="flex w-full justify-center items-center mt-5 "
+                boxShadow="lg"
+                _hover={{ boxShadow: "inner" }}
+                bg={state.isChecked ? "transparent" : "transparent"}
+                {...getCheckboxProps()}
+            >
+                <Text
+                    color="white"
+                    fontSize="xl"
+                    className="w-100%"
+                    {...getLabelProps()}
+                >
+                    {props.label}
+                </Text>
+            </Box>
+        </Link>
+    );
 }
 
 function NavPages(props) {
-  const { options, ...rest } = props;
-  const { getRootProps, getRadioProps } = useRadioGroup({
-    defaultValue: "Dashboard",
-  });
-  const group = getRootProps();
+    const { options, ...rest } = props;
+    const { getRootProps, getRadioProps } = useRadioGroup({
+        defaultValue: "Dashboard",
+    });
+    const group = getRootProps();
 
-  return (
-    <Box {...getRootProps()}>
-      {options.map((value) => {
-        const href = "/" + value.value;
-        return (
-          <NavButton
-            key={value.id}
-            href={href}
-            label={value.title}
-            border="1px solid red"
-            {...getRadioProps({ value: value.title })}
-          />
-        );
-      })}
-    </Box>
-  );
+    return (
+        <Box {...getRootProps()}>
+            {options.map((value) => {
+                const href = "/" + value.value;
+                return (
+                    <NavButton
+                        key={value.id}
+                        href={href}
+                        label={value.title}
+                        border="1px solid red"
+                        {...getRadioProps({ value: value.title })}
+                    />
+                );
+            })}
+        </Box>
+    );
 }
 
 export default function Navbar() {
-  return (
-    <div className="flex flex-initial">
-      <Box w={175} className="flex flex-col bg-blue-500 p-2">
-        <Box className="flex align-start justify-center align-start pb-10">
-          <p>Logo</p>
-        </Box>
-        <Box className="flex flex-col">
-          <NavPages
-            options={[
-              { id: 1, value: "dashboard", title: "Dashboard" },
-              { id: 2, value: "regulars", title: "Regulars" },
-              { id: 3, value: "account", title: "Account" },
-            ]}
-          />
-        </Box>
-        <Box className="flex flex-col items-center w-full">
-          <Link href="account">
-            <Avatar />
-          </Link>
-        </Box>
-      </Box>
-    </div>
-  );
+    return (
+        <div className="flex flex-initial">
+            <Box w={175} className="flex flex-col bg-blue-500 p-2">
+                <Box className="flex align-start justify-center align-start pb-10">
+                    <p>Logo</p>
+                </Box>
+                <Box className="flex flex-col">
+                    <NavPages
+                        options={[
+                            { id: 1, value: "dashboard", title: "Dashboard" },
+                            { id: 2, value: "regulars", title: "Regulars" },
+                        ]}
+                    />
+                </Box>
+                <Box className="flex flex-col items-center w-full">
+                    <Link href="account">
+                        <Avatar />
+                    </Link>
+                </Box>
+            </Box>
+        </div>
+    );
 }
